01201db7a17428d0c23a41a0a84aa913af4cc7c5
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2023-08-29 20:44:17 +0300

    build

--- src/app/ticket/components/table/table.component.ts
+++ src/app/ticket/components/table/table.component.ts
@@ -1,12 +1,10 @@
 import {
   AfterViewInit,
   Component,
-  EventEmitter,
   Input,
-  Output,
   ViewChild,
 } from '@angular/core';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { Directionality } from '@angular/cdk/bidi';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
